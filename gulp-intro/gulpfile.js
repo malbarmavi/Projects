f2c7f588ce5d1e24601897269eb41f95dfbf594f
mbarmawi <muhammadbarmawi@outlook.com>
2016-09-08 09:42:27 -0800

    Add new gulp plugins

--- gulp-intro/gulpfile.js
+++ gulp-intro/gulpfile.js
@@ -4,11 +4,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var config = require('./gulp.config');
 var autoprefixer = require('gulp-autoprefixer');
-
+var taskListig = require('gulp-task-listing');
 gulp.task('log', function () {
     console.log(config);
 });
 
+gulp.task('task-list',taskListig);
 gulp.task('hello', function hello() {
     console.log('hello from gulp.');
 });
@@ -23,3 +24,9 @@ gulp.task('css', function () {
 gulp.task('css:watch', function () {
     gulp.watch(config.sass.src, ['css']);
 });
+
+/*gulp.task("default",["log","css"]);*/
+
+gulp.task("default",["log"],function( ){
+    console.log("default");
+})
